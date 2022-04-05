a7980ddf001aa248266cf1c92bdbbe4cb60e47b9
EC2 Default User <ec2-user@ip-172-31-43-47.ap-northeast-1.compute.internal>
2022-04-05 18:30:33 +0000

    toppage in , users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,39 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
+  
+  
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger]="ログインしてしてください。"
+      redirect_to login_url
+    end
+  end
+      
+  def correct_user
+    unless current_user?(@user)
+    flash[:danger]="権限がありません。"
+    redirect_to(root_url)
+    end
+  end
+  
+  
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+  
+  
+  def admin_or_correct_user
+    unless current_user?(@user) || current_user.admin?
+     flash[:danger]="権限がありません。"
+     redirect_to(root_url)
+    end
+  end
+  
+  
+  
+    
 end
+
+  
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+  before_action :set_user,only:[:show,:edit,:update,:destroy]
+  
+  def index
+    @user=User.pagenate(page:params[:page],per_page:30)
+  end
+    
+  def show
+  end
+    
+  def new
+    @user=User.new
+  end
+  
+  
+  def create
+   @user=User.new(user_params)
+   if @user.save
+     log_in @user
+     flash[:success]='新規作成に成功しました。'
+     redirect_to user  # タスク管理では、@userとしていたが、userでいけるのでは？
+   else
+     render :new
+   end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update_attributes(user_params)
+     flash[:success]="ユーザー情報を編集しました。"
+     redirect_to user
+    else
+     render :edit
+    end
+  end
+  
+
+  def destroy
+    @user.destroy
+    flash[:danger]="#{@user.name}を削除します。"
+    redirect_to users_url
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name,:email, :password,:password_confirmation)
+    end
+    
+
+end
