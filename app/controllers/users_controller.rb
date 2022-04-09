02719941f6bfdbf57e116e43986890fa83ca746a
EC2 Default User <ec2-user@ip-172-31-43-47.ap-northeast-1.compute.internal>
2022-04-09 08:53:57 +0000

    勤怠テーブルほぼ完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   end
     
   def show
+    @worked_sum =@attendances.where.not(started_at:nil).count
     
   end
     
--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -8,4 +8,9 @@ module AttendancesHelper
   end
   
   
+  def working_times(start,finish)
+    format("%.2f", ((finish-start)/60)/60.0)
+  end
+  
+
 end
